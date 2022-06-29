d6751742bd24a2796b105f142ac3448d3a7b4f7e
Yeonchan Seong <ycseong07@gmail.com>
2022-06-29 16:42:26 +0900

    Fix error - delete parameter 'sample_size' in lightGBM

--- R/algorithms.R
+++ R/algorithms.R
@@ -185,10 +185,10 @@ naiveBayes_phi <- function(engine = "klaR",
 }
 
 
-#' Light GBM
+#' XGBoost
 #'
 #' @details
-#' Light GBM
+#' XGBoost
 #' hyperparameters: mtry, min_n, tree_depth, loss_reduction, learn_rate, sample_size
 #'
 #' @param engine engine
@@ -199,7 +199,7 @@ naiveBayes_phi <- function(engine = "klaR",
 #'
 #' @export
 
-lightGbm_phi <- function(engine = "xgboost",
+xgboost_phi <- function(engine = "xgboost",
                          mode = "classification"){
 
   result <- parsnip::boost_tree(
@@ -210,6 +210,38 @@ lightGbm_phi <- function(engine = "xgboost",
     loss_reduction = tune(),
     learn_rate = tune(),
     sample_size = tune()
+  ) %>%
+    parsnip::set_engine(engine = engine) %>%
+    parsnip::set_mode(mode = mode)
+
+  return(result)
+}
+
+#' Light GBM
+#'
+#' @details
+#' Light GBM
+#' install treesnip package by: remotes::install_github("curso-r/treesnip")
+#' hyperparameters: mtry, min_n, tree_depth, loss_reduction, learn_rate, sample_size
+#'
+#' @param engine engine
+#' @param mode mode
+#'
+#' @import parsnip
+#' @import treesnip
+#'
+#' @export
+
+lightGbm_phi <- function(engine = "lightgbm",
+                         mode = "classification"){
+
+  result <- parsnip::boost_tree(
+    mtry = tune(),
+    trees = tune(),
+    min_n = tune(),
+    tree_depth = tune(),
+    loss_reduction = tune(),
+    learn_rate = tune()
     ) %>%
     parsnip::set_engine(engine = engine) %>%
     parsnip::set_mode(mode = mode)
